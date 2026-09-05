712bcbff185b1d66c6af187af77a86e25f706baa
Oni-Kishin <erkaa2323@gmail.com>
2026-09-05 17:10:37 +0800

    feat: add ONI Firebase client

---
+++ src/integrations/firebase/client.ts
@@ -0,0 +1,16 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+export const FIREBASE_CONFIG = Object.freeze({
+  apiKey: "AIzaSyDt0DjUhafGZ2D-co3ZhZlIde_Qe1K5trw",
+  authDomain: "oni-kishin-f59b4.firebaseapp.com",
+  projectId: "oni-kishin-f59b4",
+  storageBucket: "oni-kishin-f59b4.firebasestorage.app",
+  messagingSenderId: "523789047478",
+  appId: "1:523789047478:web:c7e250d094c38abe61f817",
+});
+
+const app = getApps().length ? getApp() : initializeApp(FIREBASE_CONFIG);
+export const firebaseDb = getFirestore(app);
+export const firebaseAuth = getAuth(app);
