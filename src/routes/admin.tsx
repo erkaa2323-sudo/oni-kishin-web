fd5313a90ffa5abea30277ca16949f5337744e5e
Oni-Kishin <erkaa2323@gmail.com>
2026-09-07 16:40:07 +0800

    feat(admin): enable hardened application workflow

--- src/routes/admin.tsx
+++ src/routes/admin.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import "@/services/application-workflow";
 import { OniAdminPage } from "@/components/oni/OniAdminPage";
 
 const description =
