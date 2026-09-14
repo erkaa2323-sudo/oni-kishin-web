dc199e15ed413a891edf131820229974dcc132f6
Oni-Kishin <erkaa2323@gmail.com>
2026-09-14 21:07:50 +0800

    feat(push): activate iOS notification center

--- ios/ONIHub/ONIHubApp.swift
+++ ios/ONIHub/ONIHubApp.swift
@@ -2,6 +2,10 @@ import SwiftUI
 
 @main
 struct ONIHubApp: App {
+  init() {
+    NativeNotificationManager.shared.activate()
+  }
+
   var body: some Scene {
     WindowGroup {
       ContentView()
